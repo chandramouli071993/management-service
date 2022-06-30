d7a9fbd438313b71e26f8b3bcf75e2bffee4860a
e3cradha <accenture.radhakrishnan@consulenti.generaligroup.com>
2022-06-30 19:44:35 +0200

    configuration

--- src/main/java/com/jet/restaurants/service/openclose/JetRestaurantOpenCloseService.java
+++ src/main/java/com/jet/restaurants/service/openclose/JetRestaurantOpenCloseService.java
@@ -1,30 +1,15 @@
 package com.jet.restaurants.service.openclose;
 
-import com.jet.restaurants.service.openclose.domain.Status;
-import com.jet.restaurants.service.openclose.service.RestaurantService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
-public class JetRestaurantOpenCloseService implements CommandLineRunner {
+public class JetRestaurantOpenCloseService {
 
-	@Autowired
-	private RestaurantService restaurantService;
-
-	public static void main(String[] args) {
-		SpringApplication.run(JetRestaurantOpenCloseService.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		restaurantService.createRestaurant("McDonalds", Status.OPEN);
-		restaurantService.createRestaurant("Burger King", Status.OPEN);
-		restaurantService.createRestaurant("Pizza Hut", Status.OPEN);
-		restaurantService.createRestaurant("KFC", Status.OPEN);
-		restaurantService.createRestaurant("BurgerMe", Status.OPEN);
- 	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JetRestaurantOpenCloseService.class, args);
+    }
 
 }
--- src/main/java/com/jet/restaurants/service/openclose/configuration/KafkaConfiguration.java
+++ src/main/java/com/jet/restaurants/service/openclose/configuration/KafkaConfiguration.java
@@ -4,55 +4,56 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableKafka
 public class KafkaConfiguration {
+
+    @Value("${kafka.bootstrapServer}")
+    private String kafkaServers;
+
+    @Value("${kafka.topic.restaurant.change-status:restaurants-status}")
+    private String topicName;
 
     @Bean
     public KafkaAdmin admin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9093");
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
         return new KafkaAdmin(configs);
     }
 
     @Bean
     public NewTopic restaurantsStatus() {
-        return TopicBuilder.name("restaurants-status")
-                .partitions(1)
-                .replicas(1)
-                .build();
+        return new NewTopic(topicName, 1, (short) 1);
     }
 
     @Bean
-    public ProducerFactory<Integer, String> producerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9093");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        // See https://kafka.apache.org/documentation/#producerconfigs for more properties
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
     }
 
     @Bean
-    public KafkaTemplate<Integer, String> kafkaTemplate() {
-        return new KafkaTemplate<Integer, String>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
